0216c347afe272a5b3b5c2eab4e27615225e92e7
chinweokwu <paulcmorah@gmail.com>
2020-09-25 17:55:38 +0100

    added changes to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,16 +20,17 @@ class User < ApplicationRecord
 
   # Users who have yet to confirme friend requests
   def pending_friends
-    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+    friendships.map{|friendship| friendship.friend unless friendship.confirmed}.compact
   end
 
   # Users who have requested to be friends
   def friend_requests
-    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+    inverse_friendships.map{|friendship| friendship.user unless friendship.confirmed}.compact
   end
 
   def confirm_friend(user)
-    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    #friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship = inverse_friendships.find_by user_id: user.id
     friendship.confirmed = true
     friendship.save
   end
