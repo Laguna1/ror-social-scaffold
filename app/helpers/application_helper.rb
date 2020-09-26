9eb57697567dfc7327335caddfaedc183ba28832
chinweokwu <paulcmorah@gmail.com>
2020-09-26 21:40:23 +0100

    added changes rquired by tse

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,18 @@ module ApplicationHelper
       link_to('Like!', post_likes_path(post_id: post.id), method: :post)
     end
   end
+
+  def friend_request_helper
+    if current_user.id == params[:id].to_i
+      nil
+    elsif current_user.friend?(User.find(params[:id]))
+      'You guyz are friends already'
+    elsif current_user.pending_friends.include?(User.find(params[:id]))
+      'pls confirm your friend request'
+    elsif User.find(params[:id]).pending_friends.include?(current_user)
+      link_to 'Accept friend', friend_path(params[:id]), method: :post
+    else
+      link_to 'Send friend request', create_friend_path(params[:id]), method: :post
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show]
   resources :posts, only: [:index, :create] do
-    resources :comments, only: [:create]
-    resources :likes, only: [:create, :destroy]
-  end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :comments, only: [:create]
+  resources :likes, only: [:create, :destroy]
+  post '/friendships/update/:id', to: 'friendships#update', as: 'friend'
+  post '/friendships/create/:id', to: 'friendships#create', as: 'create_friend'
 end
