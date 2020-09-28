593ced325afd3ea61b0efba2e8233f824f9e3159
Laguna1 <lagunazoomarket@gmail.com>
2020-09-28 19:21:59 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   devise_for :users
+  post '/friendships/update/:id', to: 'friendships#update', as: 'friend'
+  post '/friendships/create/:id', to: 'friendships#create', as: 'create_friend'
 
   resources :users, only: [:index, :show]
   resources :posts, only: [:index, :create] do
   resources :comments, only: [:create]
   resources :likes, only: [:create, :destroy]
-  post '/friendships/update/:id', to: 'friendships#update', as: 'friend'
-  post '/friendships/create/:id', to: 'friendships#create', as: 'create_friend'
+  
 end
+
 end
