ce135ce0a4a2d8bf90e7398927db61d80fcc0335
Laguna1 <lagunazoomarket@gmail.com>
2020-09-28 19:50:45 +0300

    fix style errors

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,4 @@
-class FriendshipController < ApplicationController
+class FriendshipsController < ApplicationController
   def create
     friendship = current_user.friendships.create(friend_id: params[:id])
     friendship.save
