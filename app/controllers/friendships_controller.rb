0faea169809f99496c0b0b29c859475008e7a2a6
chinweokwu <paulcmorah@gmail.com>
2020-09-29 02:41:41 +0100

    added ignore button

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -9,4 +9,20 @@ class FriendshipsController < ApplicationController
     current_user.confirm_friend(User.find(params[:id]))
     redirect_to user_path(params[:id])
   end
+
+  def destroy
+    inverse_friend = User.find(params[:friend_id])
+    friend_request = inverse_friend.friendships.find(params[:id])
+  
+    friendship = current_user.friendships.find_by_friend_id(inverse_friend.id)
+    if friendship.nil?
+      friend_request.destroy
+      flash[:notice] = "friend request removed."
+    else
+      friendship.destroy
+      friend_request.destroy
+      flash[:notice] = "riendship removed."
+    end
+    redirect_to current_user
+  end
 end
