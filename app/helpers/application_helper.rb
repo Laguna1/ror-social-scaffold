6fc6aa671ffcbb75588bcaa4727d32186fc398fe
chinweokwu <paulcmorah@gmail.com>
2020-09-29 21:35:05 +0100

    added controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,22 @@ module ApplicationHelper
       link_to('Like!', post_likes_path(post_id: post.id), method: :post)
     end
   end
+
+  def user_name
+    return current_user.name unless current_user.nil?
+  end
+
+  def friend_request_helper
+    if current_user.id == params[:id].to_i
+      nil
+    elsif current_user.friend?(User.find(params[:id]))
+      'You are already friends'
+    elsif current_user.pending_friends.include?(User.find(params[:id]))
+      'Waiting for confirmation'
+    elsif User.find(params[:id]).pending_friends.include?(current_user)
+      link_to 'Accept friend', friend_path(params[:id]), method: :post
+    else
+      link_to 'Send friend request', create_friend_path(params[:id]), method: :post
+    end
+  end
 end
