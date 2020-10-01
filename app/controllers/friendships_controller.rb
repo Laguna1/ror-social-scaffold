090bbf6c9166716859b15865e9b3638608b28ab3
chinweokwu <paulcmorah@gmail.com>
2020-10-01 20:28:46 +0100

    added rspec

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -19,14 +19,4 @@ class FriendshipsController < ApplicationController
     friendship.update(confirmed: true)
     redirect_to user_path(friendship.friend_id)
   end
-  # def create
-  #     friendship = current_user.friendships.create(friend_id: params[:id])
-  #     friendship.save
-  #     redirect_to user_path(params[:id])
-  # end
-
-  #   def update
-  #     current_user.confirm_friend(User.find(params[:id]))
-  #     redirect_to user_path(params[:id])
-  #   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,18 +19,4 @@ module ApplicationHelper
   def user_name
     return current_user.name unless current_user.nil?
   end
-
-  # def friend_request_helper
-  #   if current_user.id == params[:id].to_i
-  #     nil
-  #   elsif current_user.friend?(User.find(params[:id]))
-  #     'You are already friends'
-  #   elsif current_user.pending_friends.include?(User.find(params[:id]))
-  #     'Waiting for confirmation'
-  #   elsif User.find(params[:id]).pending_friends.include?(current_user)
-  #     link_to 'Accept friend', friend_path(params[:id]), method: :post
-  #   else
-  #     link_to 'Send friend request', create_friend_path(params[:id]), method: :post
-  #   end
-  # end
 end
