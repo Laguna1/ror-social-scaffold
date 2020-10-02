4c00b99a769390a60318917d470a79f0741a942b
Laguna1 <lagunazoomarket@gmail.com>
2020-10-02 03:24:13 +0300

    fix rubocop errors

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,32 +1,32 @@
- require 'rails_helper'
+require 'rails_helper'
 
- RSpec.describe 'Log In Feature', type: :feature do
-    let(:user) { User.new(name: 'newlog', email: 'user@site.com', password: 'some123pass') }
-  
-    scenario 'User cannot log in with empty email field' do
-      user.save
-      visit new_user_session_path
-      fill_in 'Email', with: nil
-      fill_in 'Password', with: user.password
-      click_button 'Log in'
-      expect(page).to have_content('Invalid Email or password.')
-    end
-  
-    scenario 'User can log in when his account already exists' do
-      user.save
-      visit new_user_session_path
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: user.password
-      click_button 'Log in'
-      expect(page).to have_content('Signed in successfully')
-    end
-  
-    scenario 'User cannot log in with empty password field' do
-      user.save
-      visit new_user_session_path
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: nil
-      click_button 'Log in'
-      expect(page).to have_content('Invalid Email or password.')
-    end
+RSpec.describe 'Log In Feature', type: :feature do
+  let(:user) { User.new(name: 'newlog', email: 'user@site.com', password: 'some123pass') }
+
+  scenario 'User cannot log in with empty email field' do
+    user.save
+    visit new_user_session_path
+    fill_in 'Email', with: nil
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+    expect(page).to have_content('Invalid Email or password.')
   end
+
+  scenario 'User can log in when his account already exists' do
+    user.save
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+    expect(page).to have_content('Signed in successfully')
+  end
+
+  scenario 'User cannot log in with empty password field' do
+    user.save
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: nil
+    click_button 'Log in'
+    expect(page).to have_content('Invalid Email or password.')
+  end
+end
