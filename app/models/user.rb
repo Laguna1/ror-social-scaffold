553ec9806c71f493d08402f853b21650003620dc
Laguna1 <lagunazoomarket@gmail.com>
2020-10-09 00:27:34 +0300

    change friendship and user model for mutual friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,8 +15,7 @@ class User < ApplicationRecord
   has_many :pending_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "user_id"
   has_many :confirmed_friendships, -> { where confirmed: true }, class_name: "Friendship"
   has_many :inverse_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
-  # has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
-  # has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
 
   def friends
     invitation_i_sent = Friendship.where(user_id: id, confirmed: true).pluck(:friend_id)
@@ -31,5 +30,9 @@ class User < ApplicationRecord
 
   def send_invitation(user)
     friendships.create(friend_id: user.id)
+  end
+  def friends_and_own_posts
+    Post.where(user: (self.friends + self))
+    # This will produce SQL query with IN. Something like: select * from posts where user_id IN (1,45,874,43);
   end
 end
